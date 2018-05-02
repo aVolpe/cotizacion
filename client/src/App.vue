fa5bc53f7e1c3b37ff7af37ed23a0c377101912a
Arturo Volpe <arturovolpe@gmail.com>
2018-05-02 00:49:35 -0400

    feat: Improve layout

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,88 +1,45 @@
 <template>
     <v-app id="inspire">
-        <v-navigation-drawer fixed :clipped="$vuetify.breakpoint.lgAndUp" app v-model="drawer">
+        <v-navigation-drawer right floating dark v-model="drawer" app>
             <v-list dense>
-                <template v-for="item in items">
-                    <v-layout
-                            row
-                            v-if="item.heading"
-                            align-center
-                            :key="item.heading">
-                        <v-flex xs6>
-                            <v-subheader v-if="item.heading">
-                                {{ item.heading }}
-                            </v-subheader>
-                        </v-flex>
-                        <v-flex xs6 class="text-xs-center">
-                            <a href="#!" class="body-2 black--text">EDIT</a>
-                        </v-flex>
-                    </v-layout>
-                    <v-list-group
-                            v-else-if="item.children"
-                            v-model="item.model"
-                            :key="item.text"
-                            :prepend-icon="item.model ? item.icon : item['icon-alt']"
-                            append-icon="">
-                        <v-list-tile slot="activator">
-                            <v-list-tile-content>
-                                <v-list-tile-title>
-                                    {{ item.text }}
-                                </v-list-tile-title>
-                            </v-list-tile-content>
-                        </v-list-tile>
-                        <v-list-tile
-                                v-for="(child, i) in item.children"
-                                :key="i"
-                                @click=""
-                        >
-                            <v-list-tile-action v-if="child.icon">
-                                <v-icon>{{ child.icon }}</v-icon>
-                            </v-list-tile-action>
-                            <v-list-tile-content>
-                                <v-list-tile-title>
-                                    {{ child.text }}
-                                </v-list-tile-title>
-                            </v-list-tile-content>
-                        </v-list-tile>
-                    </v-list-group>
-                    <v-list-tile v-else @click="" :key="item.text">
-                        <v-list-tile-action>
-                            <v-icon>{{ item.icon }}</v-icon>
-                        </v-list-tile-action>
-                        <v-list-tile-content>
-                            <v-list-tile-title>
-                                {{ item.text }}
-                            </v-list-tile-title>
-                        </v-list-tile-content>
-                    </v-list-tile>
-                </template>
+                <v-list-tile>
+                    <v-list-tile-action>
+                        <v-icon>home</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>Home</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                    <v-list-tile-action>
+                        <v-icon>contact_mail</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>Contact</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
             </v-list>
         </v-navigation-drawer>
-        <v-toolbar color="blue darken-3" dark app :clipped-left="$vuetify.breakpoint.lgAndUp" fixed>
-            <v-toolbar-title class="ml-0 pl-3">
-                <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-                <span class="hidden-sm-and-down">Cotizaciones del Paraguay</span>
-            </v-toolbar-title>
+        <v-toolbar color="indigo" dark fixed app>
             <v-spacer></v-spacer>
-            <v-btn icon>
-                <v-icon>apps</v-icon>
-            </v-btn>
-            <v-btn icon>
-                <v-icon>notifications</v-icon>
-            </v-btn>
-            <v-btn icon large>
-                <v-avatar size="32px" tile>
-                    <img src="https://vuetifyjs.com/static/doc-images/logo.svg" alt="Vuetify">
-                </v-avatar>
-            </v-btn>
+            <v-toolbar-title>Cotizaciones</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
         </v-toolbar>
         <v-content>
             <v-container fluid fill-height>
-                <v-layout>
-                    <router-view/>
+                <v-layout justify-center align-center>
+                    <v-flex text-xs-center xs12 md7>
+                        <router-view/>
+                    </v-flex>
                 </v-layout>
             </v-container>
         </v-content>
+        <v-footer color="primary" app>
+            <v-spacer></v-spacer>
+            <span class="white--text">&copy; 2018</span>
+            <v-spacer></v-spacer>
+        </v-footer>
     </v-app>
 </template>
 <script lang="ts">
@@ -95,24 +52,8 @@
         components: {},
     })
     export default class App extends Vue {
-        dialog = false;
-        drawer = null;
-        items = [
-            {icon: 'contacts', text: 'Cotizacion diaria'},
-            {icon: 'history', text: 'Cotizacion histórica'},
-            {icon: 'content_copy', text: 'Casas de cambio'},
-            {
-                icon: 'keyboard_arrow_up',
-                'icon-alt': 'keyboard_arrow_down',
-                text: 'Acerca de',
-                model: true,
-                children: [
-                    {icon: 'add', text: 'Info'},
-                    {icon: 'add', text: 'Licencias usadas'},
-                    {icon: 'add', text: 'Fuentes de datos'},
-                ]
-            },
-        ]
+
+        drawer: boolean = false;
     }
 </script>
 
--- client/src/main.ts
+++ client/src/main.ts
@@ -7,11 +7,11 @@ import colors from 'vuetify/es5/util/colors'
 
 Vue.config.productionTip = false;
 Vue.use(Vuetify, {
-    // theme: {
-    //     primary: colors.red.darken1, // #E53935
-    //     secondary: colors.red.lighten4, // #FFCDD2
-    //     accent: colors.indigo.base // #3F51B5
-    // }
+    theme: {
+        primary: colors.indigo.darken3, // #E53935
+        secondary: colors.red.lighten4, // #FFCDD2
+        accent: colors.indigo.base // #3F51B5
+    }
 });
 
 new Vue({
