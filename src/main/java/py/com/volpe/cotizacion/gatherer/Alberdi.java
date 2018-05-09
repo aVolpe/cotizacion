b5dfd3c721c6dc509e3bdd2b61b36b8db68b5026
Arturo Volpe <arturovolpe@gmail.com>
2018-05-08 21:22:54 -0400

    feat: Add cambios Alberdi data

--- src/main/java/py/com/volpe/cotizacion/gatherer/Alberdi.java
+++ src/main/java/py/com/volpe/cotizacion/gatherer/Alberdi.java
@@ -114,6 +114,8 @@ public class Alberdi implements Gatherer {
 				pb.setName(name);
 				pb.setRemoteCode(name);
 				pb.setPlace(p);
+				addMetaData(pb);
+
 				p.getBranches().add(pb);
 			});
 
@@ -122,6 +124,68 @@ public class Alberdi implements Gatherer {
 			throw new AppException(500, "cant parse the result of alberdi ws", e);
 		}
 
+	}
+
+	private void addMetaData(PlaceBranch pb) {
+		switch (pb.getName()) {
+			case "asuncion":
+				pb.setName("Asunción");
+				pb.setLatitude(-25.281411);
+				pb.setLongitude(-57.6375917);
+				pb.setImage("https://lh5.googleusercontent.com/p/AF1QipMxA2Nv-mtjAzqti1pUgd_Bt3z8nfbBBizklGEw=w408-h244-k-no");
+				pb.setPhoneNumber("(021) 447.003 / (021) 447.004");
+				pb.setSchedule("07:45 horas a 17:00 horas de Lunes a Viernes, 07:45 horas a 12:00 horas Sábados");
+				pb.setEmail("matriz@cambiosalberdi.com");
+				break;
+			case "villamorra":
+				pb.setName("Villa Morra");
+				pb.setPhoneNumber("(021) 609.905 / (021) 609.906");
+				pb.setSchedule("08:00 horas a 17:00 horas de Lunes a Viernes, 08:00 horas a 12:00 horas Sábados");
+				pb.setImage("https://lh5.googleusercontent.com/p/AF1QipP9gq7gRfgXTFGdQFGJYWLZGq_9SSLJ_pKYN4Uk=w408-h306-k-no");
+				pb.setLatitude(-25.2962143);
+				pb.setLongitude(-57.5766948);
+				break;
+			case "sanlo":
+				pb.setName("San Lorenzo");
+				pb.setImage("https://lh5.googleusercontent.com/p/AF1QipM0yx3fvWQArt0kY6EwyaaAsgX1jYRy3OuIobjr=w408-h725-k-no");
+				pb.setLatitude(-25.3459184);
+				pb.setLongitude(-57.5151255);
+				pb.setPhoneNumber("Teléfonos: (021) 571.215 / (021) 571.216");
+				pb.setSchedule("08:00 horas a 17:00 horas de Lunes a Viernes, 08:00 horas a 12:00 horas Sábados");
+				break;
+			case "salto":
+				pb.setName("SALTO DEL GUAIRÁ");
+				pb.setLatitude(-24.055276);
+				pb.setLongitude(-54.3246485);
+				pb.setPhoneNumber("Teléfonos: (046) 243.158 / (046) 243.159");
+				pb.setSchedule("08:00 horas a 16:00 horas de Lunes a Viernes, 07:30 horas a 11:30 horas Sábados");
+				break;
+			case "cde":
+				pb.setName("SUCURSAL 1 CDE");
+				pb.setLatitude(-25.5098204);
+				pb.setLongitude(-54.6164127);
+				pb.setPhoneNumber("Teléfonos: (061) 500.135 / (061) 500.417");
+				pb.setSchedule("07:00 horas a 17:00 horas de Lunes a Viernes, 07:00 horas a 12:00 horas Sábados");
+				break;
+			case "cde2":
+				pb.setName("CDE KM 4");
+				pb.setLatitude(-25.5095271);
+				pb.setLongitude(-54.6485326);
+				pb.setPhoneNumber("Teléfonos: (061) 571.540 / (061) 571.536");
+				pb.setSchedule("07:00 horas a 17:00 horas de Lunes a Viernes, 07:00 horas a 12:00 horas Sábados");
+				break;
+			case "enc":
+				pb.setName("ENCARNACIÓN");
+				pb.setLatitude(-27.3314553);
+				pb.setLongitude(-55.8670186);
+				pb.setImage("https://lh5.googleusercontent.com/p/AF1QipOAtjZef_kGv14qJ4h68Rt4CKOxxwYXPJW30BUY=w408-h306-k-no");
+				pb.setSchedule("07:45 horas a 17:00 horas de Lunes a Viernes, 07:45 horas a 12:00 horas Sábados");
+				pb.setPhoneNumber("Teléfonos: (071) 205.154 / (071) 205.120 / (071) 205.144");
+				break;
+			default:
+				log.warn("Unkonw branch {} of alberdi", pb.getName());
+
+		}
 	}
 
 	private Map<String, List<ExchangeData>> getParsedData() throws IOException {
