5a9421e2f0b47ea1fd5467a2dafcb29b0ccf661c
Arturo Volpe <arturovolpe@gmail.com>
2018-05-10 19:58:49 -0400

    feat: Add sitemap.xml generator

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,19 @@
+const SitemapPlugin = require('sitemap-webpack-plugin').default;
+
+const today = new Date().toISOString().replace(/T.*/, '')
+
+const paths = [
+  {
+    path: '/', lastMod: today, priority: '0.8', changeFreq: 'hourly',
+    path: '/#/licenses', lastMod: today, priority: '0.2', changeFreq: 'monthly',
+    path: '/#/swagger', lastMod: today, priority: '0.2', changeFreq: 'monthly',
+  }
+];
+
 module.exports = {
   configureWebpack: {
+    plugins: [
+      new SitemapPlugin('https://cotizaciones.volpe.com.py', paths)
+    ]
   }
 }
