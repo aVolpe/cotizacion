9d2f96137466b9f922f7c89cc994b4aec0a4f546
Arturo Volpe <arturovolpe@gmail.com>
2018-05-10 22:40:36 -0400

    fix: Use double quotes to make codacy happy

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,17 +1,17 @@
-var SitemapPlugin = require('sitemap-webpack-plugin').default;
+var SitemapPlugin = require("sitemap-webpack-plugin").default;
 
-var today = new Date().toISOString().replace(/T.*/, '');
+var today = new Date().toISOString().replace(/T.*/, "");
 
 var paths = [
-    {path: '/', lastMod: today, priority: '0.8', changeFreq: 'hourly'},
-    {path: '/#/licenses', lastMod: today, priority: '0.1', changeFreq: 'monthly'},
-    {path: '/#/swagger', lastMod: today, priority: '0.1', changeFreq: 'monthly'}
+    {path: "/", lastMod: today, priority: "0.8", changeFreq: "hourly"},
+    {path: "/#/licenses", lastMod: today, priority: "0.1", changeFreq: "monthly"},
+    {path: "/#/swagger", lastMod: today, priority: "0.1", changeFreq: "monthly"}
 ];
 
 module.exports = {
     configureWebpack: {
         plugins: [
-            new SitemapPlugin('https://cotizaciones.volpe.com.py', paths)
+            new SitemapPlugin("https://cotizaciones.volpe.com.py", paths)
         ]
     }
 }
