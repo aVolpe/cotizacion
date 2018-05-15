0fa38769df5a540d93ca477449a4c6e10c626061
Arturo Volpe <arturovolpe@gmail.com>
2018-05-15 18:11:49 -0400

    feat: Add prometheous support trough the actuator

---
+++ src/main/java/py/com/volpe/cotizacion/WSHelper.java
@@ -0,0 +1,8 @@
+package py.com.volpe.cotizacion;
+
+/**
+ * @author Arturo Volpe
+ * @since 5/15/18
+ */
+public class WSHelper {
+}
---
+++ src/test/java/py/com/volpe/cotizacion/gatherer/AlberdiTest.java
@@ -0,0 +1,11 @@
+package py.com.volpe.cotizacion.gatherer;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Arturo Volpe
+ * @since 5/15/18
+ */
+public class AlberdiTest {
+
+}
