01155d30ded68e0e566cd9b48efb9c91d60d1849
Arturo Volpe <arturovolpe@gmail.com>
2018-05-14 21:07:56 -0400

    test: Remove spring runner for simple place controller test

--- src/test/java/py/com/volpe/cotizacion/controller/PlaceControllerTest.java
+++ src/test/java/py/com/volpe/cotizacion/controller/PlaceControllerTest.java
@@ -1,13 +1,7 @@
 package py.com.volpe.cotizacion.controller;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.context.junit4.SpringRunner;
 import py.com.volpe.cotizacion.GathererManager;
 
 import static org.junit.Assert.assertEquals;
@@ -16,24 +10,20 @@ import static org.junit.Assert.assertEquals;
  * @author Arturo Volpe
  * @since 5/14/18
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PlaceControllerTest {
 
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    @MockBean
-    private GathererManager manager;
     @Test
     public void testCalls() {
+
+        GathererManager manager = Mockito.mock(GathererManager.class);
+        PlaceController pc = new PlaceController(manager);
 
         Mockito.when(manager.doQuery(Mockito.anyString())).thenReturn("a");
         Mockito.when(manager.init(Mockito.anyString())).thenReturn("a");
 
 
-        assertEquals(restTemplate.getForEntity("/api/places/init?code=a", String.class).getBody(), "a");
-        assertEquals(restTemplate.getForEntity("/api/places/doQuery?code=a", String.class).getBody(), "a");
+        assertEquals(pc.doQuery("a"), "a");
+        assertEquals(pc.init("a"), "a");
 
 
 
