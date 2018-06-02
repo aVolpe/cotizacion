cac9710ff2df142835e41e237009e27f4d7d45d4
Arturo Volpe <arturovolpe@gmail.com>
2018-06-02 03:33:37 -0400

    feat: Move alberdi branch data to a json file

--- src/main/java/py/com/volpe/cotizacion/gatherer/Alberdi.java
+++ src/main/java/py/com/volpe/cotizacion/gatherer/Alberdi.java
@@ -15,6 +15,8 @@ import py.com.volpe.cotizacion.domain.QueryResponse;
 import py.com.volpe.cotizacion.domain.QueryResponseDetail;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -68,6 +70,12 @@ public class Alberdi implements Gatherer {
         return Long.parseLong(number.replace(".", ""));
     }
 
+    /**
+     * We read a file from the class path called "alberdi_branch_data", the file has the
+     * important information about all the branches
+     *
+     * @return the place
+     */
     @Override
     public Place build() {
 
@@ -76,83 +84,28 @@ public class Alberdi implements Gatherer {
 
         Place place = new Place("Cambios Alberdi", CODE);
 
-        Map<String, List<ExchangeData>> result = getParsedData();
+        Map<String, PlaceBranch> result = getBranchData();
 
-        result.keySet().forEach(name -> place.addBranch(buildBranch(name, place)));
+        place.setBranches(new ArrayList<>(result.values()));
 
         return place;
 
     }
 
-    private PlaceBranch buildBranch(String code, Place p) {
-        PlaceBranch.PlaceBranchBuilder pb = PlaceBranch.builder()
-                .name(code)
-                .remoteCode(code)
-                .place(p);
+    private Map<String, PlaceBranch> getBranchData() {
 
-        switch (code) {
-            case "asuncion":
-                return pb.name("Asunción")
-                        .latitude(-25.281411)
-                        .longitude(-57.6375917)
-                        .image("https://lh5.googleusercontent.com/p/AF1QipMxA2Nv-mtjAzqti1pUgd_Bt3z8nfbBBizklGEw=w408-h244-k-no")
-                        .phoneNumber("(021) 447.003 / (021) 447.004")
-                        .schedule("07:45 horas a 17:00 horas de Lunes a Viernes, 07:45 horas a 12:00 horas Sábados")
-                        .email("matriz@cambiosalberdi.com")
-                        .build();
-            case "villamorra":
-                return pb.name("Villa Morra")
-                        .phoneNumber("(021) 609.905 / (021) 609.906")
-                        .schedule("08:00 horas a 17:00 horas de Lunes a Viernes, 08:00 horas a 12:00 horas Sábados")
-                        .image("https://lh5.googleusercontent.com/p/AF1QipP9gq7gRfgXTFGdQFGJYWLZGq_9SSLJ_pKYN4Uk=w408-h306-k-no")
-                        .latitude(-25.2962143)
-                        .longitude(-57.5766948)
-                        .build();
-            case "sanlo":
-                return pb.name("San Lorenzo")
-                        .image("https://lh5.googleusercontent.com/p/AF1QipM0yx3fvWQArt0kY6EwyaaAsgX1jYRy3OuIobjr=w408-h725-k-no")
-                        .latitude(-25.3459184)
-                        .longitude(-57.5151255)
-                        .phoneNumber("(021) 571.215 / (021) 571.216")
-                        .schedule("08:00 horas a 17:00 horas de Lunes a Viernes, 08:00 horas a 12:00 horas Sábados")
-                        .build();
-            case "salto":
-                return pb.name("Salto del Guairá")
-                        .latitude(-24.055276)
-                        .longitude(-54.3246485)
-                        .phoneNumber("(046) 243.158 / (046) 243.159")
-                        .schedule("08:00 horas a 16:00 horas de Lunes a Viernes, 07:30 horas a 11:30 horas Sábados")
-                        .build();
-            case "cde":
-                return pb.name("Sucursal 1 CDE")
-                        .latitude(-25.5098204)
-                        .longitude(-54.6164127)
-                        .phoneNumber("(061) 500.135 / (061) 500.417")
-                        .schedule("07:00 horas a 17:00 horas de Lunes a Viernes, 07:00 horas a 12:00 horas Sábados")
-                        .build();
-            case "cde2":
-                return pb.name("CDE KM 4")
-                        .latitude(-25.5095271)
-                        .longitude(-54.6485326)
-                        .phoneNumber("(061) 571.540 / (061) 571.536")
-                        .schedule("07:00 horas a 17:00 horas de Lunes a Viernes, 07:00 horas a 12:00 horas Sábados")
-                        .build();
-            case "enc":
-                return pb.name("Encarnación")
-                        .latitude(-27.3314553)
-                        .longitude(-55.8670186)
-                        .image("https://lh5.googleusercontent.com/p/AF1QipOAtjZef_kGv14qJ4h68Rt4CKOxxwYXPJW30BUY=w408-h306-k-no")
-                        .schedule("07:45 horas a 17:00 horas de Lunes a Viernes, 07:45 horas a 12:00 horas Sábados")
-                        .phoneNumber("(071) 205.154 / (071) 205.120 / (071) 205.144")
-                        .build();
-            default:
-                log.warn("Unknown branch {} of {}", code, CODE);
-                return pb.build();
+        InputStream fileIS = getClass().getClassLoader().getResourceAsStream("alberdi_branch_data.json");
 
+        try {
+            return buildMapper()
+                    .readValue(fileIS, new TypeReference<Map<String, PlaceBranch>>() {
+                    });
+        } catch (IOException e) {
+            throw new AppException(500, "Can't read branch file", e);
         }
     }
 
-    Map<String, List<ExchangeData>> getParsedData() {
+    protected Map<String, List<ExchangeData>> getParsedData() {
         try {
             String queryResult = helper.getDataWithoutSending(WS_URL);
             return buildMapper()
@@ -185,6 +138,7 @@ public class Alberdi implements Gatherer {
                 return null;
         }
     }
+
 
     @Data
     @JsonIgnoreProperties(ignoreUnknown = true)
--- src/test/java/py/com/volpe/cotizacion/CotizacionAppTests.java
+++ src/test/java/py/com/volpe/cotizacion/CotizacionAppTests.java
@@ -1,7 +1,9 @@
 package py.com.volpe.cotizacion;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,8 +14,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class CotizacionAppTests {
 
+    @Autowired
+    GathererManager manager;
+
     @Test
     public void contextLoads() {
+
+        Assert.assertNotNull(manager);
     }
 
 }
