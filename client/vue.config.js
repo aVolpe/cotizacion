7db234d32cce79f4aa7e5a34c60e66ed8e7318e8
Arturo Volpe <arturovolpe@gmail.com>
2018-09-24 22:59:28 -0400

    chore: fix pwa config

--- client/vue.config.js
+++ client/vue.config.js
@@ -36,14 +36,6 @@ module.exports = {
     msTileColor: "#283593",
     appleMobileWebAppCapable: "yes",
     appleMobileWebAppStatusBarStyle: "black",
- 
-    // configure the workbox plugin
-    workboxPluginMode: "InjectManifest",
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: "dev/sw.js",
-      // ...other Workbox options...
-    }
   }
 };
 
